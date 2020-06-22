e64aa1713cc9769ab1dd2076426903cf986532e5
mdvv85009 <mdvv85009@gmail.com>
2020-06-23 00:38:52 +0800

    [Feature] Add ThirdSceneState.cs

---
+++ State/ThirdSceneState.cs
@@ -0,0 +1,24 @@
+﻿namespace State
+{
+    public class ThirdSceneState : ISceneState
+    {
+        public ThirdSceneState() : base("ThirdSceneState")
+        {
+           
+        }
+
+        public override void StateBegin()
+        {
+
+        }
+
+        public override void StateUpdate()
+        {
+
+        }
+
+        public override void StateEnd()
+        {
+        }
+    }
+}
